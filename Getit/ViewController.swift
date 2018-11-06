3a9bd9a96f991646d169f2d3d0a2edcaae3ba602
bhavsingh2017 <bhavkirs@usc.edu>
2018-11-06 01:55:44 -0800

    Initial Commit

---
+++ Getit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Getit
+//
+//  Created by Bhav Singh on 11/6/18.
+//  Copyright © 2018 AppDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
